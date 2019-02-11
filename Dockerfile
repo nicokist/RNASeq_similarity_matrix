fc26b80594a326e4b1ec7149430a369d2510d748
Nico Kist <nico@kist.nl>
2019-02-11 10:23:06 +0000

    changes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,21 @@
 FROM ubuntu:16.04
 RUN apt-get update
-RUN apt-get install -y python
-COPY resources/* /app/
+RUN apt-get install -y python parallel default-jre
+COPY app/* /app/
+WORKDIR /app
+
+##Samtools is included as a binary in the repository, gatk and freebayes were too large for github so are obtained properly.
+
+RUN git clone --recursive git://github.com/ekg/freebayes.git
+WORKDIR /app/freebayes
+RUN make
+
+WORKDIR /app
+RUN wget https://github.com/broadinstitute/gatk/releases/download/4.1.0.0/gatk-4.1.0.0.zip
+RUN unzip gatk-4.1.0.0.zip
+
+RUN wget ftp://ftp.ensembl.org/pub/release-84/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
+
+
 WORKDIR /data/
 CMD ["python", "/app/RNASeq_sample_confusion.py"]
--- app/RNASeq_sample_confusion.py
+++ app/RNASeq_sample_confusion.py
@@ -14,7 +14,7 @@ call_and_check("/app/samtools merge --threads 30 -r -R 1 bam_files.merged_chr1.b
 call_and_check("/app/samtools view -H bam_files.merged_chr1.bam | grep -v '^@RG' > bam_files.merged_chr1.new_header")
 call_and_check("find bam_files/*.bam | /app/generate_RGs.py >> bam_files.merged_chr1.new_header")
 call_and_check("/app/samtools reheader bam_files.merged_chr1.new_header bam_files.merged_chr1.bam > bam_files.merged_chr1.header_withRG.bam")
-call_and_check('java -jar /app/gatk-package-4.0.11.0-local.jar MarkDuplicates --INPUT bam_files.merged_chr1.header_withRG.bam --OUTPUT bam_files.merged_chr1.header_withRG.MarkDuplicates.bam --CREATE_INDEX -M MarkDuplicates.metrics --VALIDATION_STRINGENCY LENIENT')
+call_and_check('java -jar /app/gatk-4.1.0.0/gatk-package-4.1.0.0-local.jar MarkDuplicates --INPUT bam_files.merged_chr1.header_withRG.bam --OUTPUT bam_files.merged_chr1.header_withRG.MarkDuplicates.bam --CREATE_INDEX -M MarkDuplicates.metrics --VALIDATION_STRINGENCY LENIENT')
 
 call_and_check("gzip -d /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz")
 
