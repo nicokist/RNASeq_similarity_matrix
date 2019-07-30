e8e8c66fceb6fe0868ba14df3bfe5c595b3f8f6d
Nico Kist <nico@kist.nl>
2019-07-30 19:09:01 +0000

    Fixes, the for loop completes without error.

--- Dockerfile
+++ Dockerfile
@@ -11,26 +11,28 @@ RUN gzip -d /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
 RUN wget -q https://github.com/broadinstitute/gatk/releases/download/4.1.0.0/gatk-4.1.0.0.zip
 RUN unzip gatk-4.1.0.0.zip
 
+RUN wget -q https://github.com/broadinstitute/picard/releases/download/2.20.4/picard.jar
+
 RUN wget -q ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/00-common_all.vcf.gz
 
 #R
 RUN apt-get -y install software-properties-common
 RUN add-apt-repository "deb http://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/"
-RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9 
 RUN apt-get update
 RUN apt-get install -y r-base r-base-dev
 
 
 ##Samtools is included as a binary in the repository, gatk and freebayes were too large for github so are obtained properly.
 RUN git clone --recursive git://github.com/ekg/freebayes.git
-WORKDIR /app/freebayes
-RUN make
-RUN cp /app/freebayes/bin/freebayes /usr/local/bin
-
-WORKDIR /app/freebayes/vcflib
-RUN make
-RUN cp bin/* /usr/local/bin/
-
+#WORKDIR /app/freebayes
+#RUN make
+#RUN cp /app/freebayes/bin/freebayes /usr/local/bin
+#
+#WORKDIR /app/freebayes/vcflib
+#RUN make
+#RUN cp bin/* /usr/local/bin/
+#
 COPY app/* /app/
 RUN chmod -R 777 /app
 
--- app/RNASeq_sample_confusion.py
+++ app/RNASeq_sample_confusion.py
@@ -37,7 +37,7 @@ for root in roots:
     )
 
 
-    call_and_check("samtools view -b bam_files/{0}.bam 1 > intermediate_files/{0}.chr1.bam".format(root))
+    call_and_check("/app/samtools view -b bam_files/{0}.bam 1 > intermediate_files/{0}.chr1.bam".format(root))
 
 
     call_and_check("java -jar /app/picard.jar AddOrReplaceReadGroups I=intermediate_files/{0}.chr1.bam O=intermediate_files/{0}.chr1.RGs.bam CREATE_INDEX=true RGID='XX' RGLB='XX' RGPL='XX' RGPU='XX' RGSM=${0}".format(root))
@@ -48,11 +48,11 @@ for root in roots:
 
     call_and_check("/app/gatk-4.1.0.0/gatk BaseRecalibrator -R /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa -L 1 --known-sites /app/00-common_all.vcf.gz -I intermediate_files/{0}.chr1.RGs.no_duplicates.split_cigar.bam -O intermediate_files/{0}.recalibration.table".format(root))
 
-    call_and_check("/app/gatk-4.1.0.0/gatk ApplyBQSR -R /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa -L 1 --bqsr-recal-file intermediate_files/{0}.recalibration.table -I intermediate_files/{0}.chr1.RGs.no_duplicates.split_cigar.bam -O intermediate_files/${FILE_NAME}.chr1.RG.NoDup.Split.BQSR.bam".format(root))
+    call_and_check("/app/gatk-4.1.0.0/gatk ApplyBQSR -R /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa -L 1 --bqsr-recal-file intermediate_files/{0}.recalibration.table -I intermediate_files/{0}.chr1.RGs.no_duplicates.split_cigar.bam -O intermediate_files/{0}.chr1.RG.NoDup.Split.BQSR.bam".format(root))
 
-    call_and_check("/app/gatk-4.1.0.0/gatk HaplotypeCaller -R /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa -I intermediate_files/{0}.chr1.RG.NoDup.Split.BQSR.bam -L 1 --dont-use-soft-clipped-bases --standard-min-confidence-threshold-for-calling 20.0 --dbsnp /app/00-common_all.vcf.gz -O /intermediate_files/{}.chr1.vcf.gz -A QualByDepth -A Coverage -A ClippingRankSumTest -A DepthPerSampleHC".format(root))
+    call_and_check("/app/gatk-4.1.0.0/gatk HaplotypeCaller -R /app/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa -I intermediate_files/{0}.chr1.RG.NoDup.Split.BQSR.bam -L 1 --dont-use-soft-clipped-bases --standard-min-confidence-threshold-for-calling 20.0 --dbsnp /app/00-common_all.vcf.gz -O intermediate_files/{0}.chr1.vcf.gz -A QualByDepth -A Coverage -A ClippingRankSumTest -A DepthPerSampleHC".format(root))
       
-    call_and_check("/app/gatk-4.1.0.0/gatk VariantFiltration -V intermediate_files/{0}.chr1.vcf.gz -window 35 -cluster 3 --filter-name "FS" --filter-expression "FS > 30.0" --filter-name "QD" --filter-expression "QD < 2.0" -O intermediate_files/{0}.chr1.filtered.vcf.gz".format(root))
+    call_and_check('/app/gatk-4.1.0.0/gatk VariantFiltration -V intermediate_files/{0}.chr1.vcf.gz -window 35 -cluster 3 --filter-name "FS" --filter-expression "FS > 30.0" --filter-name "QD" --filter-expression "QD < 2.0" -O intermediate_files/{0}.chr1.filtered.vcf.gz'.format(root))
 
 call_and_check("bcftools merge intermediate_files/*.chr1.filtered.vcf.gz > intermediate_files/merged.chr1.vcf.gz")
 
